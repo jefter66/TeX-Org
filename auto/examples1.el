f828db7705c5c0750eec223b32a16ee5e00246f4
jefterrsantiago <jeftersantiago@usp.br>
2021-08-24 17:59:28 -0300

    done

---
+++ auto/examples1.el
@@ -0,0 +1,49 @@
+(TeX-add-style-hook
+ "examples1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "listings")
+   (LaTeX-add-labels
+    "sec:orgc7835cb"
+    "sec:orgf5f4950"
+    "sec:org13cc4b2"
+    "sec:orge0fb19c"
+    "sec:orgbd21877"
+    "sec:org02e04d7"
+    "sec:org6b3a86d"
+    "sec:org9d2d8f2"
+    "sec:orgb775e9d"
+    "sec:org9dfde29"))
+ :latex)
+
---
+++ auto/usefullExamples.el
@@ -0,0 +1,62 @@
+(TeX-add-style-hook
+ "usefullExamples"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=0.2in") ("babel" "brazil" "english") ("animate" "controls")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "listings"
+    "blindtext"
+    "geometry"
+    "subcaption"
+    "titling"
+    "titlesec"
+    "amsfonts"
+    "multicol"
+    "gensymb"
+    "siunitx"
+    "colortbl"
+    "hhline"
+    "babel"
+    "indentfirst"
+    "tikz"
+    "animate"
+    "color"
+    "epigraph")
+   (LaTeX-add-labels
+    "sec:org01858ad"
+    "sec:org9e60297"
+    "sec:org67a5b83"
+    "sec:org88e5ebb"
+    "sec:org68782da"
+    "sec:org9b96457"))
+ :latex)
+
